e110bb1716f58b4414f2632474034467ac2ba818
Eric Lamb <eric@mithra62.com>
2025-12-27 15:31:26 -0700

    adds tags layer

--- app/Models/User.php
+++ app/Models/User.php
@@ -11,11 +11,12 @@ use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
 use Spatie\Permission\Traits\HasRoles;
 use Laravolt\Avatar\Avatar;
+use Spatie\Tags\HasTags;
 
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
-    use HasFactory, Notifiable, HasApiTokens, HasRoles;
+    use HasFactory, Notifiable, HasApiTokens, HasRoles, HasTags;
 
     /**
      * The attributes that are mass assignable.
