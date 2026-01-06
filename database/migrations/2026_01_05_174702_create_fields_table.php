bf81b6349316566046d994eb914f6fbc4a55ee0c
Eric Lamb <eric@mithra62.com>
2026-01-05 18:01:29 -0700

    adds fields migration

---
+++ database/migrations/2026_01_05_174702_create_fields_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('fields', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug')->index();
+            $table->string('label')->nullable();
+            $table->string('type');
+            $table->text('instructions')->nullable();
+            $table->json('settings')->nullable();
+            $table->boolean('hidden')->default(false);
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('fields');
+    }
+};
