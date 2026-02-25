6d2b36bb01a2f75fcb5ba1efeade55af27347305
Eric Lamb <eric@mithra62.com>
2026-02-24 20:32:09 -0700

    renames field object

--- app/Models/Field/Group.php
+++ app/Models/Field/Group.php
@@ -2,23 +2,26 @@
 
 namespace App\Models\Field;
 
+use App\Models\Field;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Group extends Model
 {
     protected $fillable = [
-        'request_route',
-        'method',
-        'request_payload',
-        'request_headers',
-        'response_payload',
-        'response_headers',
-        'response_status_code',
-        'user_id',
+        'name',
+        'slug',
+        'description'
     ];
 
     /**
      * @var string
      */
     protected $table = 'field_groups';
+
+    public function fields(): BelongsToMany
+    {
+        return $this->belongsToMany(Field::class, 'field_groups_fields')->withPivot('group_id', 'field_id');
+    }
 }
