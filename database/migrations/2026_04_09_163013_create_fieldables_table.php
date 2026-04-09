751407b687622a6cb7c6aa02a25dbb933b384941
Eric Lamb <eric@mithra62.com>
2026-04-09 16:37:40 -0700

    adds field groupables and fieldables migrations

---
+++ database/migrations/2026_04_09_163013_create_fieldables_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('fieldables', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('field_id')
+                ->constrained('fields')
+                ->cascadeOnDelete();
+            $table->unsignedBigInteger('fieldable_id');
+            $table->string('fieldable_type');
+            $table->timestamps();
+            $table->index(['fieldable_id', 'fieldable_type'], 'f_type_index');
+            $table->unique([
+                'field_id',
+                'fieldable_id',
+                'fieldable_type',
+            ], 'f_unique');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('fieldables');
+    }
+};
