6fe6de24ec231c826887628307e38ff1f159033f
Eric Lamb <eric@mithra62.com>
2026-04-20 22:52:40 -0700

    starts account api route

--- app/Http/Requests/Account/EditPasswordRequest.php
+++ app/Http/Requests/Account/EditPasswordRequest.php
@@ -35,19 +35,6 @@ class EditPasswordRequest extends FormRequest
     {
         return [
             'current_password.required' => 'You must enter your current password.',
-            'email.unique' => 'This email is already registered. Try logging in instead.',
-            'roles.required' => 'You must select at least one role.',
-        ];
-    }
-
-    /**
-     * @return string[]
-     */
-    public function attributes(): array
-    {
-        return [
-            'name' => 'full name',
-            'email' => 'email address',
         ];
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\v1\User;
 use App\Http\Controllers\Api\v1\Entries;
+use App\Http\Controllers\Api\v1\Account;
 use Illuminate\Support\Facades\Route;
 use App\Http\Middleware\LogRequestResponse;
 
@@ -12,4 +13,8 @@ Route::middleware('auth:sanctum')->prefix('v1')->group(function () {
 
     Route::apiResource('entries', Entries::class, ['names' => 'api.v1.entries'])
         ->middleware(LogRequestResponse::class);
+
+    Route::get('/account', [Account::class, 'show'])
+        ->middleware(LogRequestResponse::class)
+        ->name('api.v1.account.show');
 });
