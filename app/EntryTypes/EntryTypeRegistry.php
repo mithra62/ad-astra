c71ee7dc600f6ea20cc9be1eda1b90bf9ea8cf55
Eric Lamb <eric@mithra62.com>
2026-04-22 17:58:52 -0700

    adds caching to registry items

--- app/EntryTypes/EntryTypeRegistry.php
+++ app/EntryTypes/EntryTypeRegistry.php
@@ -7,18 +7,36 @@ use RuntimeException;
 
 class EntryTypeRegistry
 {
+    /** @var array<string, AbstractEntryType> */
+    private array $handleCache = [];
+
+    /** @var array<int, AbstractEntryType> */
+    private array $idCache = [];
+
     public function resolveByHandle(string $handle): AbstractEntryType
     {
-        $record = EntryTypeRecord::where('handle', $handle)
-            ->with(['entryGroup', 'fieldLayout.tabs.elements.field.fieldType'])
-            ->firstOrFail();
+        if (! isset($this->handleCache[$handle])) {
+            $record = EntryTypeRecord::where('handle', $handle)
+                ->with(['entryGroup', 'fieldLayout.tabs.elements.field.fieldType'])
+                ->firstOrFail();
 
-        return $this->instantiate($record);
+            $instance = $this->instantiate($record);
+            $this->handleCache[$handle] = $instance;
+            $this->idCache[$record->getKey()] = $instance;
+        }
+
+        return $this->handleCache[$handle];
     }
 
     public function resolveByRecord(EntryTypeRecord $record): AbstractEntryType
     {
-        return $this->instantiate($record);
+        $id = $record->getKey();
+
+        if (! isset($this->idCache[$id])) {
+            $this->idCache[$id] = $this->instantiate($record);
+        }
+
+        return $this->idCache[$id];
     }
 
     private function instantiate(EntryTypeRecord $record): AbstractEntryType
