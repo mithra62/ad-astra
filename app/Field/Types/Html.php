57640f8646cbb8d38e936575b5eae4cd7732cec6
Eric Lamb <eric@mithra62.com>
2026-04-30 15:41:30 -0700

    adds HTML FieldType

---
+++ app/Field/Types/Html.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Field\Types;
+
+use App\Field\AbstractField;
+
+class Html extends AbstractField
+{
+    protected string $handle = 'html';
+    protected string $name = 'HTML';
+
+    protected array $rules = [
+        'numeric',
+    ];
+
+    public function storageColumn(): string
+    {
+        return 'value_text';
+    }
+
+    public function render(array $params): string
+    {
+        return view('_fields.html', $params)->render();
+    }
+}
