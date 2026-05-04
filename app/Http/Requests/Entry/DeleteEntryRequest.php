8c18b57ee873c505b37a0e9adb08ffba14ed9c97
Eric Lamb <eric@mithra62.com>
2026-05-04 15:17:11 -0700

    more

--- app/Http/Requests/Entry/DeleteEntryRequest.php
+++ app/Http/Requests/Entry/DeleteEntryRequest.php
@@ -23,7 +23,6 @@ class DeleteEntryRequest extends FormRequest
     {
         return [
             'confirm_removal.required' => 'You must confirm removal.',
-            'confirm_removal.accepted' => 'You must confirm removal.',
         ];
     }
 }
