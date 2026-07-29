ccdb74930b5ce828cbddb41121736f601921ec10
Eric Lamb <eric@mithra62.com>
2026-07-28 21:55:59 -0700

    adds more mixins

--- packages/core/src/Http/Resources/Api/UserResource.php
+++ packages/core/src/Http/Resources/Api/UserResource.php
@@ -6,6 +6,9 @@ use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 use OpenApi\Attributes as OA;
 
+/**
+ * @mixin \AdAstra\Models\User
+ */
 #[OA\Schema(
     schema: 'User',
     title: 'User',
